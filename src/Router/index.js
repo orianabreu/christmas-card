3d1198dd7ba16d80a2c0fd081cfdc2ebb618c0e5
Oriana Abreu <oriabreu.v@gmail.com>
2020-12-21 19:26:28 +0100

    fixed default path

--- src/Router/index.js
+++ src/Router/index.js
@@ -4,7 +4,7 @@ import Home from '../pages/Home';
 export default function CustomRouter() {
     return (
         <BrowserRouter>
-            <Route exact path='/index.html/:id' component={Home} />
+            <Route exact path='/:id' component={Home} />
             <Route component={Home} />
         </BrowserRouter>
     )
